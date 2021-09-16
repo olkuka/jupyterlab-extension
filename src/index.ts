50a0fa4f8c0247163fbd26c04379b9bbaadb0033
Aleksandra Kukawka <kukawkaa@gmail.com>
2021-09-16 13:27:03 +0200

    add MultiQC button to launcher

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,47 @@ import {
   JupyterFrontEnd,
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
+import { MainAreaWidget } from '@jupyterlab/apputils';
+import { ILauncher } from '@jupyterlab/launcher';
 
 import { requestAPI } from './handler';
+
+import TestWidget from './TestWidget';
+
+const activate = (app: JupyterFrontEnd, launcher: ILauncher) => {
+  // blank widget
+  const content = new TestWidget();
+  const widget = new MainAreaWidget({ content });
+  widget.id = 'multiqc-jupyterlab';
+  widget.title.label = 'MultiQC extension';
+  widget.title.closable = true;
+
+  const command: string = 'multiqc:open';
+
+  // add to launcher
+  app.commands.addCommand(command, {
+    label: 'MultiQC',
+    caption: 'Run MultiQC',
+    execute: () => {
+      if (!widget.isAttached) {
+        app.shell.add(widget, 'main');
+      }
+      app.shell.activateById(widget.id);
+    },
+  });
+
+  launcher.add({ command, category: 'Test' });
+
+  requestAPI<any>('get_example')
+    .then(data => {
+      console.log(data);
+    })
+    .catch(reason => {
+      console.error(
+        `The jupyterlab-multiqc server extension appears to be missing.\n${reason}`
+      );
+    });
+}
 
 /**
  * Initialization data for the jupyterlab-multiqc extension.
@@ -11,19 +50,8 @@ import { requestAPI } from './handler';
 const plugin: JupyterFrontEndPlugin<void> = {
   id: 'jupyterlab-multiqc:plugin',
   autoStart: true,
-  activate: (app: JupyterFrontEnd) => {
-    console.log('JupyterLab extension jupyterlab-multiqc is activated!');
-
-    requestAPI<any>('get_example')
-      .then(data => {
-        console.log(data);
-      })
-      .catch(reason => {
-        console.error(
-          `The jupyterlab-multiqc server extension appears to be missing.\n${reason}`
-        );
-      });
-  }
+  requires: [ILauncher],
+  activate: activate
 };
 
 export default plugin;
